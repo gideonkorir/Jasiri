ac50dc09a78187e1d765ac7aebb33d3ad56cce47
Gideon Korir <gideon@m-kopa.com>
2017-10-18 18:16:28 +0300

    Added tests for V2 serializer

---
+++ src/Jasiri.Tests/ManualClock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jasiri.Tests
+{
+    class ManualClock
+    {
+        readonly DateTimeOffset startTime;
+        DateTimeOffset currentTime;
+
+        public DateTimeOffset StartTime => startTime;
+
+        public ManualClock(DateTimeOffset startTime)
+        {
+            this.startTime = startTime;
+            currentTime = startTime;
+        }
+
+        public void Move(TimeSpan timeSpan)
+            => currentTime = currentTime.Add(timeSpan);
+
+        public void Reset()
+            => currentTime = startTime;
+
+        public DateTimeOffset Now()
+            => currentTime;
+
+        public static ManualClock FromUtcNow
+            => new ManualClock(DateTimeOffset.UtcNow);
+    }
+}
