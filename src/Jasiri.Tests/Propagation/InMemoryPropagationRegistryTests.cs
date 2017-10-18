60241399060dfb126f26a03a3a3f4897320e5529
Gideon Korir <gideon@m-kopa.com>
2017-10-18 16:26:12 +0300

    Added propagation registry tests + refactor files into smaller ones

---
+++ src/Jasiri.Tests/Propagation/InMemoryPropagationRegistryTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jasiri.Tests.Propagation
+{
+    using Jasiri.Propagation;
+    using OpenTracing.Propagation;
+    using Xunit;
+
+    public class InMemoryPropagationRegistryTests
+    {
+        [Fact]
+        public void PropagatorThrowsWhenRegisteringNullInstance()
+        {
+            var registry = new InMemoryPropagationRegistry();
+            var ex = Record.Exception(() => registry.Register(Formats.HttpHeaders, null));
+            Assert.NotNull(ex);
+            Assert.IsType<ArgumentNullException>(ex);
+        }
+
+        [Fact]
+        public void RegistrationIsSuccessfulForNonNullInstance()
+        {
+            var registry = new InMemoryPropagationRegistry();
+            registry.Register(Formats.HttpHeaders, new B3Propagator());
+        }
+
+        [Fact]
+        public void TryGetReturnsRegisteredPropagator()
+        {
+            var registry = new InMemoryPropagationRegistry();
+            registry.Register(Formats.HttpHeaders, new B3Propagator());
+            Assert.True(registry.TryGet(Formats.HttpHeaders, out var prop));
+            Assert.NotNull(prop);
+            Assert.IsType<B3Propagator>(prop);
+        }
+
+        [Fact]
+        public void TryGetFailsForUnRegisteredPropagator()
+        {
+            var registry = new InMemoryPropagationRegistry();
+            Assert.False(registry.TryGet(Formats.HttpHeaders, out var _));
+            Assert.False(registry.TryGet(Formats.TextMap, out var _));
+
+            registry.Register(Formats.TextMap, new B3Propagator());
+            Assert.True(registry.TryGet(Formats.TextMap, out var _));
+            Assert.False(registry.TryGet(Formats.HttpHeaders, out var _));
+        }
+    }
+}
---
+++ src/Jasiri/Propagation/InMemoryPropagationRegistry.cs
@@ -0,0 +1,36 @@
+﻿using OpenTracing.Propagation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jasiri.Propagation
+{
+
+    public class InMemoryPropagationRegistry : IPropagationRegistry
+    {
+        readonly Dictionary<string, Dictionary<Type, object>> propagators = new Dictionary<string, Dictionary<Type, object>>();
+        public bool TryGet<TCarrier>(Format<TCarrier> format, out IPropagator<TCarrier> propagator)
+        {
+            if (propagators.TryGetValue(format.Name, out var typeMap) && typeMap.TryGetValue(typeof(TCarrier), out var obj))
+            {
+                propagator = (IPropagator<TCarrier>)obj;
+                return true;
+            }
+            propagator = null;
+            return false;
+        }
+
+        public void Register<TCarrier>(Format<TCarrier> format, IPropagator<TCarrier> propagator)
+        {
+            if (propagator == null)
+                throw new ArgumentNullException(nameof(propagator));
+            if (propagators.TryGetValue(format.Name, out var typeMap))
+                typeMap[typeof(TCarrier)] = propagator;
+            else
+                propagators.Add(format.Name, new Dictionary<Type, object>()
+                {
+                    [typeof(TCarrier)] = propagator
+                });
+        }
+    }
+}
