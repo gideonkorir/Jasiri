eb0375b4310884d8ed94b63dfef05fd335e829a6
Gideon Korir <gideon@m-kopa.com>
2017-10-18 20:33:33 +0300

    Added RateLimitingSampler + Tests

---
+++ src/Jasiri.Tests/Sampling/RateLimitingSamplerTests.cs
@@ -0,0 +1,46 @@
+﻿using Jasiri.Sampling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace Jasiri.Tests.Sampling
+{
+    public class RateLimitingSamplerTests
+    {
+        [Fact]
+        public void RateLimiterAllowsMaxUnitsPerInterval()
+        {
+            var sampler = new RateLimitingSampler(new Rate()
+            {
+                Units = 1,
+                Interval = TimeSpan.FromDays(1)
+            });
+            Thread.Sleep(20); //give us time to init
+            using (sampler)
+            {
+                Assert.True(sampler.Sample("op", 5));
+                Assert.False(sampler.Sample("op", 5));
+            }
+        }
+
+        [Fact]
+        public void RateLimiterSetsCorrectTags()
+        {
+            var rate = new Rate()
+            {
+                Units = 1,
+                Interval = TimeSpan.FromDays(1)
+            };
+            var sampler = new RateLimitingSampler(rate);
+            Thread.Sleep(20);
+            using (sampler)
+            {
+                var sample = sampler.Sample("op", 56);
+                Assert.Equal("ratelimiting", sample.Tags["sampler"]);
+                Assert.Equal(rate.ToString(), sample.Tags["sampler-arg"]);
+            }
+        }
+    }
+}
---
+++ src/Jasiri/Sampling/RateLimitingSampler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Jasiri.Sampling
+{
+    public class Rate
+    {
+        public uint Units { get; set; }
+        public TimeSpan Interval { get; set; }
+
+        public override string ToString()
+            => $"{Units} units in {Interval.TotalMilliseconds} ms";
+    }
+    public class RateLimitingSampler : ISampler, IDisposable
+    {
+        readonly Rate rate;
+        readonly Timer timer;
+        readonly Dictionary<string, string> tags;
+
+        long units = 0;
+
+        public RateLimitingSampler(Rate rate)
+        {
+            this.rate = rate ?? throw new ArgumentNullException(nameof(rate));
+            tags = new Dictionary<string, string>()
+            {
+                ["sampler"] = "ratelimiting",
+                ["sampler-arg"] = rate.ToString()
+            };
+            timer = new Timer(AddUnits, null, TimeSpan.Zero, Timeout.InfiniteTimeSpan);
+        }
+
+        void AddUnits(object arg)
+        {
+            Interlocked.Exchange(ref units, rate.Units);
+            timer.Change(rate.Interval, Timeout.InfiniteTimeSpan);
+        }
+
+        public SamplingStatus Sample(string operationName, ulong traceId)
+        {
+            var newValue = Interlocked.Decrement(ref units);
+            return new SamplingStatus(newValue >= 0, tags);
+        }
+
+        public void Dispose()
+            => timer.Dispose();
+    }
+}
