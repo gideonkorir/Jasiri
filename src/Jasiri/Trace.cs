e8bffb4d219ca152835d72a1636191a5295d3c23
Gideon Korir <gideon@m-kopa.com>
2017-11-12 12:29:45 +0300

    Added noop tracer and Trace class

---
+++ src/Jasiri/Trace.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jasiri
+{
+    public static class Trace
+    {
+        static readonly ITracer noOpTracer = new NoOpTracer();
+        static ITracer tracer = noOpTracer;
+        public static ITracer Tracer
+        {
+            get => tracer;
+            set => tracer = value ?? noOpTracer;
+        }
+    }
+}
