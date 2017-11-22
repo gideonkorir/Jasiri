700ab699bcdc7fa9a6d0713e32b323239ff7ba77
Gideon Korir <gideon@m-kopa.com>
2017-11-22 19:08:06 +0300

    Add methods to simplify my life

--- src/Jasiri/Trace.cs
+++ src/Jasiri/Trace.cs
@@ -13,5 +13,30 @@ namespace Jasiri
             get => tracer;
             set => tracer = value ?? noOpTracer;
         }
+
+        public static IZipkinSpan NewSpan(string operationName, bool forceNew = false)
+            => Tracer.NewSpan(operationName, forceNew);
+
+        public static IZipkinSpan NewSpan(string operationName, SpanContext parentContext)
+            => Tracer.NewSpan(operationName, parentContext);
+
+        public static void Inject(string injectorKey, SpanContext spanContext, Propagation.IPropagatorMap propagatorMap)
+        {
+            if(Tracer.PropagationRegistry.TryGet(injectorKey, out var propagator))
+            {
+                propagator.Inject(spanContext, propagatorMap);
+            }
+            else
+            { 
+                throw new InvalidOperationException($"Propagator for key {injectorKey} not found");
+            }
+        }
+
+        public static SpanContext Extract(string extractorKey, Propagation.IPropagatorMap propagatorMap)
+        {
+            if (Tracer.PropagationRegistry.TryGet(extractorKey, out var propagator))
+                return propagator.Extract(propagatorMap);
+            throw new InvalidOperationException($"Propagator for key {extractorKey} not found");
+        }
     }
 }
