7143d736a3db85fc419c2d4d73ef45ba352f7ede
Gideon Korir <gideon@m-kopa.com>
2017-11-22 12:35:08 +0300

    Added net 452 support

---
+++ src/Jasiri/AsyncLocal.cs
@@ -0,0 +1,25 @@
+﻿#if NET452
+using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+
+namespace Jasiri
+{
+    class AsyncLocal<T>
+    {
+        readonly string name = Guid.NewGuid().ToString();
+        public T Value
+        {
+            get
+            {
+                var obj = CallContext.LogicalGetData(name);
+                if (ReferenceEquals(null, obj))
+                    return default(T);
+                return (T)obj;
+            }
+            set => CallContext.LogicalSetData(name, value);
+        }
+    }
+}
+#endif
